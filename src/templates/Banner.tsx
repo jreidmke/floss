505eab1b984145ba0659dc93faff010257ea6dde
Andrew <adaiza@crescendocollective.com>
2021-07-16 07:34:05 -0700

    added content to text

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -10,12 +10,29 @@ const Banner = () => (
       title="Click here to see our options or to speak with one of our operators"
       subtitle="Start your Free Trial."
       button={
-        <Link href="https://creativedesignsguru.com/category/nextjs/">
+        <Link href="/">
           <a>
             <Button>Get Started</Button>
           </a>
         </Link>
       }
+    />
+    <div
+      className="
+          hidden
+          md:block
+          overflow-hidden
+          bg-bookmark-purple
+          rounded-l-full
+          absolute
+          h-80
+          w-2/4
+          top-32
+          right-0
+          lg:
+          -bottom-28
+          lg:-right-36
+        "
     />
   </Section>
 );
