3712be24ff7b51b52f7848d6a1409e084c84b9e9
Andrew <adaiza@crescendocollective.com>
2021-07-16 08:46:29 -0700

    base

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -7,7 +7,7 @@ import { Section } from '../layout/Section';
 const Banner = () => (
   <Section>
     <CTABanner
-      title="Click here to see our options or to speak with one of our operators"
+      title="Testimonials"
       subtitle="Start your Free Trial."
       button={
         <Link href="https://creativedesignsguru.com/category/nextjs/">
