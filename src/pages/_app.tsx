984ffbb32fe6382e1c589f3a0e35d995d4830f27
James Reid <jamesreid@jamess-mbp.home>
2021-07-19 13:31:11 -0500

    npm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,22 @@ import { AppProps } from 'next/app';
 
 import '../styles/main.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
+// eslint-disable-next-line import/order
+import { useEffect } from 'react';
+
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-NSM8BT4',
+};
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
-);
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
