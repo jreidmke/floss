41987c09ddd5c815d20a7c46f9d442185e121ff0
James Reid <jamesreid@jamess-mbp.home>
2021-07-22 08:07:44 -0500

    remvoe unnecesary ga code

--- googleAnalytics.js
+++ googleAnalytics.js
@@ -1,11 +1,11 @@
-// log the pageview with their URL
-export const pageview = (url) => {
-  global.window.gtag('config', 'G-531B9RH73H', {
-    page_path: url,
-  });
-};
+// // log the pageview with their URL
+// export const pageview = (url) => {
+//   global.window.gtag('config', 'G-531B9RH73H', {
+//     page_path: url,
+//   });
+// };
 
-// log specific events happening.
-export const event = ({ action, params }) => {
-  global.window.gtag('event', action, params);
-};
+// // log specific events happening.
+// export const event = ({ action, params }) => {
+//   global.window.gtag('event', action, params);
+// };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-danger */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 import { AppConfig } from '../utils/AppConfig';
 
@@ -9,7 +8,7 @@ class MyDocument extends Document {
     return (
       <Html lang={AppConfig.locale}>
         <Head>
-          <Script src="https://www.googletagmanager.com/gtag/js?id=G-531B9RH73H" />
+          {/* <Script src="https://www.googletagmanager.com/gtag/js?id=G-531B9RH73H" />
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -18,7 +17,7 @@ class MyDocument extends Document {
               gtag('js', new Date()); gtag('config', 'G-531B9RH73H');
               `,
             }}
-          />
+          /> */}
         </Head>
 
         <body>
