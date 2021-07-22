a881a247b83850749f34fccd76e887146044457b
James Reid <jamesreid@jamess-mbp.home>
2021-07-22 08:03:17 -0500

    remvoe unnecesary ga code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,9 @@ import { AppProps } from 'next/app';
 
 import '../styles/main.css';
 
-// eslint-disable-next-line import/order
-import { useEffect } from 'react';
-
-import TagManager from 'react-gtm-module';
-
-const tagManagerArgs = {
-  gtmId: 'GTM-NSM8BT4',
-};
-
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
-  }, []);
-
+const MyApp = ({ Component, pageProps }: AppProps) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
-};
+  <Component {...pageProps} />
+);
 
 export default MyApp;
